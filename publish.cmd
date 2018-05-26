a203e9525c7e28693288c985be5dbb36ecac8704
“yourname” <youremail@gmail.com>
2018-05-27 06:33:08 +0800

    init

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=llpbot-Web-Deploy.pubxml -p:Password=8DXdNy3gJyT5kywwurCbDGa5ms9SrJ8CQeqhSRpvHb5FdHEegJxqWf8MMtgs
+
